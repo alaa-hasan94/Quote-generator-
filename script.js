0f0fec12564a7f25616df555d1cf5ee9c6d218fb
Alaa hasaan <46227897+alaa-hasan94@users.noreply.github.com>
2023-10-09 16:49:57 +0300

    check code on phone

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const qouteContainer = document.getElementById("qoute-container")
 const qouteText = document.getElementById("qoute")
 const authorText = document.getElementById("author")
-const twitterBtn = document.getElementById("twitter")
+const shareBtn = document.getElementById("share")
 const newQouteBtn = document.getElementById("new-qoute")
 const loader = document.getElementById("loader")
+
 let data =[];
 
 // show loader
@@ -39,7 +40,7 @@ complete()
 }
 // get qoute from api
 async function getQoute() {
-    loading
+    loading()
     const apiurl ="https://jacintodesign.github.io/quotes-api/data/quotes.json"
     try {
         const response = await fetch (apiurl);
@@ -51,12 +52,45 @@ async function getQoute() {
 }
 
 // x qoute
-function xQoute (){
-    const xUrl = `https://twitter.com/intent/tweet?text=${qouteText.textContent} - ${authorText.textContent}`;
-    window.open(xUrl , '_blank');
-}
+// function xQoute (){
+//     const xUrl = `https://twitter.com/intent/tweet?text=${qouteText.textContent} - ${authorText.textContent}`;
+//     window.open(xUrl , '_blank');
+// }
+ // Function to open the share dialog
+ function togglePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = (popup.style.display === "block") ? "none" : "block";
+  }
+
+  // Function to share on Facebook
+  function shareFacebook() {
+    var shareUrl = `https://www.facebook.com/sharer/sharer.php?t=${encodeURIComponent(authorText.textContent)}`;
+    window.open(shareUrl, '_blank');
+    closePopup();
+  }
+
+  // Function to share on Twitter
+  function shareTwitter() {
+    var shareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(qouteText.textContent)} - ${encodeURIComponent(authorText.textContent)}`;
+    window.open(shareUrl, '_blank');
+    closePopup();
+  }
+
+  // Function to share on LinkedIn
+  function shareLinkedIn() {
+    var shareUrl = "https://www.linkedin.com/shareArticle?url=" + encodeURIComponent(window.location.href);
+    window.open(shareUrl, '_blank');
+    closePopup();
+  }
+
+  // Function to close the popup dialog
+  function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+  }
 // event lisener
 newQouteBtn.addEventListener('click',newQoute);
-twitterBtn.addEventListener('click',xQoute);
+// twitterBtn.addEventListener('click',xQoute);
+shareBtn.addEventListener('click', togglePopup);
 // onload
 getQoute();
